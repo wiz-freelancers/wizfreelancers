81e7c74ddc08b04d57a8aad96c917c1e865bc9e0
harshit-nwt <harshit.kumar@networkwize.com>
2025-02-02 07:08:52 -0500

    css fix

--- wiz/src/index.js
+++ wiz/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './index.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
